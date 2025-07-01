ca140f18326fa536e8a3ecf5bf49fa04707ae16e
Trupti505 <trupti.patwardhan2@gmail.com>
2025-07-01 20:26:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+
+# Load model and vectorizer
+model = joblib.load("sentiment_model.pkl")
+vectorizer = joblib.load("tfidf_vectorizer.pkl")
+
+# Define FastAPI app
+app = FastAPI()
+
+# Define request schema
+class ReviewRequest(BaseModel):
+    review: str
+
+# Prediction endpoint
+@app.post("/predict")
+def predict_sentiment(data: ReviewRequest):
+    review_text = data.review
+    X = vectorizer.transform([review_text])
+    prediction = model.predict(X)[0]
+    label = "positive" if prediction == 1 else "negative"
+    return {"sentiment": label}
